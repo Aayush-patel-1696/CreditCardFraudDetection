940a60690227a0f03351522e63cddfab0edea666
Atheesh Krishnan <atheesh.krishnan@outlook.com>
2024-03-15 15:48:31 -0400

    run1

--- src/datasampling.py
+++ src/datasampling.py
@@ -27,27 +27,33 @@ class dataSampling:
     def get_data_without_sample(self):
         return self.X_train, self.X_test, self.y_train, self.y_test
     
+    # Methods to Oversample
+    # NOTES: add ADASYN
+
     def get_data_smote(self):
         sm = SMOTE(random_state=self.random_state)
         X_sm, y_sm = sm.fit_resample(self.X_train, self.y_train)
         return X_sm,self.X_test,y_sm,self.y_test
 
-    def get_data_near_miss(self,sampling_strategy=0.1):
-        nm = NearMiss(sampling_strategy=sampling_strategy)
-        X_nm,y_nm = nm.fit_resample(self.X_train,self.y_train)
-        return X_nm,self.X_test,y_nm,self.y_test
-       
     def get_data_random_over_sample(self):
         os =  RandomOverSampler(random_state=self.random_state)
         X_os,y_os = os.fit_resample(self.X_train,self.y_train) 
         return X_os,self.X_test,y_os,self.y_test
+    
+    # Methods to Undersample
+    # NOTES: add cluster centroids, tomek links
+
+    def get_data_near_miss(self,sampling_strategy=0.1):
+        nm = NearMiss(sampling_strategy=sampling_strategy)
+        X_nm,y_nm = nm.fit_resample(self.X_train,self.y_train)
+        return X_nm,self.X_test,y_nm,self.y_test
 
     def print_class_percentage(self,X_train,X_test,y_train,y_test):
-        print("----Train class count------")
+        print("=====Train class count=====")
         y_tr_cnts = y_train.value_counts()
         print(y_tr_cnts)
         print(y_tr_cnts/sum(y_tr_cnts))
-        print("----Test class count------")
+        print("\n=====Test class count=====")
         y_ts_cnts = y_test.value_counts()
         print(y_ts_cnts)
         print(y_ts_cnts/sum(y_ts_cnts))
