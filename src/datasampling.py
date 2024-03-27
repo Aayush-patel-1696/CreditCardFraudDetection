5d1f548b444be5ef29e66c0205fc644c1693a020
Atheesh Krishnan <atheesh.krishnan@outlook.com>
2024-03-27 19:49:37 -0400

    misc

--- src/datasampling.py
+++ src/datasampling.py
@@ -28,8 +28,6 @@ class dataSampling:
         return self.X_train, self.X_test, self.y_train, self.y_test
     
     # Methods to Oversample
-    # NOTES: add ADASYN
-
     def get_data_smote(self):
         sm = SMOTE(random_state=self.random_state)
         X_sm, y_sm = sm.fit_resample(self.X_train, self.y_train)
@@ -41,13 +39,12 @@ class dataSampling:
         return X_os,self.X_test,y_os,self.y_test
     
     # Methods to Undersample
-    # NOTES: add cluster centroids, tomek links
-
     def get_data_near_miss(self,sampling_strategy=0.1):
         nm = NearMiss(sampling_strategy=sampling_strategy)
         X_nm,y_nm = nm.fit_resample(self.X_train,self.y_train)
         return X_nm,self.X_test,y_nm,self.y_test
 
+    # Results
     def print_class_percentage(self,X_train,X_test,y_train,y_test):
         print("=====Train class count=====")
         y_tr_cnts = y_train.value_counts()
